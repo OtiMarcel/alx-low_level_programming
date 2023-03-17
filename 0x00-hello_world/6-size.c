92a6ed9bea87368beec0b130419a8eea103a80bf
OtiMarcel <otimarcel@gmail.com>
2023-03-17 04:40:19 +0000

    puts

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+*A program that prints
+*the size of various types
+*/
+
+int main(void)
+{
+printf("Size of a char: %lu\n", (unsigned long)sizeof(c));
+printf("Size of an int: %lu\n", (unsigned long)sizeof(i));
+printf("Size of a long int: %lu\n", (signed long)sizeof(li));
+printyf("Size of a long long int: %lu\n" (long long)sizeof(lld));
+printf("Size of a float: %lu\n" (float)sizeof(f));
+
+return (0);
+}
