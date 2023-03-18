5b7977bab0a1dc214510be1e469cc3620379a0de
OtiMarcel <otimarcel@gmail.com>
2023-03-18 22:44:22 +0000

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Entry
+*
+* Return: 0 (success)
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
