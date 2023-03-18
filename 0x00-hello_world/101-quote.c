a13317c98d78346d177a4ca731592288877bbcd6
OtiMarcel <otimarcel@gmail.com>
2023-03-18 22:25:48 +0000

    hello

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - printing out without
+* using printf 0r puts function
+*
+*Return 1 (Success)
+*/
+int main(void)
+	{
+		write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+	return (1);
+	}
