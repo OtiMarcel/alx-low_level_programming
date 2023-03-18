2e50948de10925107abbf0f9f71791f0da0ec6ac
OtiMarcel <otimarcel@gmail.com>
2023-03-18 21:52:44 +0000

    Numberz

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Entry
+*
+* Return: 0 (Success)
+*/
+int main(void)
+{
+	int NUM;
+
+	for (NUM = 0; NUM < 10; NUM++)
+	printf("%d", NUM);
+
+	printf("\n");
+
+	return (0);
+}
