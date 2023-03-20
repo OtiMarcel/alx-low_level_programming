4cfeae9c8096a642c2d3432b541c2feaffa951ee
OtiMarcel <otimarcel@gmail.com>
2023-03-20 21:26:57 +0000

    combination of two digits with no repition

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Entry
+* prints all possible different combinations of two digits.
+*The two digits must be different
+*
+* Return: all combinations ending with 89 and retuen 0 (success)
+*/
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+				putchar((i % 10) + '0');
+				putchar((j % 10) + '0');
+
+				if (i == 8 && j == 9)
+					continue;
+
+					putchar(',');
+					putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
