7e1a0023c86a09597d2139bec600ba4203500e81
OtiMarcel <otimarcel@gmail.com>
2023-03-21 00:35:31 +0000

    software is eating the world

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,6 +13,7 @@ int main(void)
 	for (i = 0; i <= 98; i++)
 {
 	for (j = i + 1; j <= 99; j++)
+{
 	putchar((i / 10) + '0');
 	putchar((i % 10) + '0');
 	putchar(' ');
@@ -24,6 +25,7 @@ int main(void)
 
 	putchar(',');
 	putchar(' ');
+}
 }
 	putchar('\n');
 
