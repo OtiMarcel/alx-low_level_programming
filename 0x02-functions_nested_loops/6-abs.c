0a00123c5471e939fbf4ede57a1111cc8afcb67f
OtiMarcel <otimarcel@gmail.com>
2023-03-22 20:21:01 +0000

    absolute value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,15 +1,18 @@
 #include "main.h"
 /**
 * _abs - displays absolute value of a number
-* @int: is a single input
+* @n: is a single input
 *
 * Return: integer
 */
 int _abs(int n)
 {
 	if (n < 0)
+{
 	return (n * -1);
-
+}
 	else
-	return n;
+{
+	return (n);
+}
 }
