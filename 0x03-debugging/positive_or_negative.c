59bbf1c53d3657ec3970538eb0728143bb4e6bcf
OtiMarcel <otimarcel@gmail.com>
2023-03-23 19:15:56 +0000

    zero

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -5,22 +5,21 @@
 
 /**
 * main - Entry
-* @i: single input 
+* @i: single input
 * Return:  i is positve or zero or negative (Success)
 */
 void positive_or_negative(int i)
 {
-        if (i > 0)
-        {
-                printf("%d is positive\n", i);
-        }
-        else if (i == 0)
-        {
-                printf("%d is zero\n", i);
-        }
-        else
-        {
-                printf("%d is negative\n", i);
-        }
-    
+	if (i > 0)
+{
+	printf("%d is positive\n", i);
+}
+	else if (i == 0)
+{
+	printf("%d is zero\n", i);
+}
+	else
+{
+	printf("%d is negative\n", i);
+}
 }
