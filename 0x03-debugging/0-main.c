b5f9b53aee30088b6cc04a85463f6bfa50dc1de8
OtiMarcel <otimarcel@gmail.com>
2023-03-23 18:35:20 +0000

    testing zero

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* main - prints out the sign of zero
+*
+*Return: 0, positive or negative (success)
+*/
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
