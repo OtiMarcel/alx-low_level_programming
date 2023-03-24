b6c2ddfb4093666900e89a292b2b81471ebd3402
OtiMarcel <otimarcel@gmail.com>
2023-03-24 03:03:45 +0000

    remaining days

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -10,26 +10,7 @@
 */
 void print_remaining_days(int month, int day, int year)
 {
- if ((year % 2 == 0 || year % 200 == 0) && !(year % 100 == 0))
-{
-	if (month >= 1 && day >= 50)
-{
-	day++;
-}
 
 	printf("Day of the year: %d\n", day);
 	printf("Remaining days: %d\n", 366 - day);
-}
-	else
-{
-	if (month == 1 && day == 30)
-{
-	printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-}
-	else
-{
-	printf("Day of the year: %d\n", day);
-	printf("Remaining days: %d\n", 366 - day);
-}
-}
 }
