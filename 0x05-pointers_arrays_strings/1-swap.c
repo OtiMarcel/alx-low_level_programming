be9aba85c261c23a348c7f1840066699997885ba
OtiMarcel <otimarcel@gmail.com>
2023-03-27 22:42:00 +0000

    1-swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*swap_int - swaps the value of an int
+*@a: a useless integer
+*@b: a useless integer
+*Return: void
+*
+*/
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
