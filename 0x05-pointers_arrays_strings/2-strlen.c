22ef4fcfe19e70a88c001d2ab26f3ee2064ce365
OtiMarcel <otimarcel@gmail.com>
2023-03-27 23:05:29 +0000

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+* _strlen - function returning length
+* of a string
+*@s: string
+*
+* Return : length of a string
+*/
+int _strlen(char *s)
+{
+	return strlen(s);
+}
