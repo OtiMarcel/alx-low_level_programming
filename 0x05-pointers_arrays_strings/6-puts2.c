46651e125f3774d35a69d569bfea6bd6a4a59b14
OtiMarcel <otimarcel@gmail.com>
2023-03-28 22:37:23 +0000

    puts 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+/**
+* puts2 - prints out every other character 
+* of a string
+*@str: input string
+*
+* Return: void
+*/
+void puts2(char *str)
+{
+	int i, len;
+
+	len = strlen(str);
+
+	for (i = 0; i < len; i++)
+{
+	_puts(str[i];
+}
+}
