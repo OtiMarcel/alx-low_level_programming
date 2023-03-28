36dcbb1cb1486aa72ad748f73d4e2c4badaf685f
OtiMarcel <otimarcel@gmail.com>
2023-03-28 00:43:19 +0000

    dtring reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,13 +8,16 @@
 */
 void print_rev(char *s)
 {
-	int i;
+	int c = 0;
 
-	int count = 0;
+	while (s[c] != '\0')
+{
+	c++;
+}
 
-	for  (i = 0; s[i] != '\0'; i++)
-	count++;
-
-	for (i = count; i >= 0; i--)
-	_putchar(s[i]);
+	for (c = c - 1; c>= 0; c--)
+{
+	_putchar(s[c]);
+}
+	_putchar('\n');
 }
