619799817cf7d22ee6c944ae1e2cffb858dedb4e
OtiMarcel <otimarcel@gmail.com>
2023-04-11 12:46:49 +0000

    0-puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*_puts_recussion - prints a string
+*@s: string to be printed
+*
+*Retuirn: void
+*/
+void _puts_recursion(char *s)
+{
+	if (s[0] == '\0')
+{
+	_putchar('\n');
+}
+	else
+{
+	_putchar(s[0]);
+	_puts_recursion(s + 1);
+}
+}
